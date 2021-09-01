8908ee5cc4821736e9179e280c9811df1cb4200a
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-02 01:22:59 +0600

    updated

--- js/book.js
+++ js/book.js
@@ -1,3 +1,6 @@
+const searchNumber = document.getElementById('searchNumber');
+searchNumber.style.display = 'none';
+
 const getBooks = () => {
     const url = 'http://openlibrary.org/search.json?q=javascript';
     fetch(url)
@@ -8,6 +11,7 @@ const getBooks = () => {
 const showSearchedBooks = () => {
     const searchInput = document.getElementById('search');
     const searchName = searchInput.value;
+    searchInput.value = '';
     const url = `http://openlibrary.org/search.json?q=${searchName}`;
     fetch(url)
     .then(res => res.json())
@@ -18,15 +22,14 @@ const bookInfo = (book) => {
     const books = document.getElementById('books');
     books.textContent = '';
     console.log(book.docs.length);
+    const searchResults = book.docs.length;
+    searchNumber.style.display = 'block';
+    searchNumber.innerHTML = `
+    <h5 class="mx-4 my-3">${searchResults} results found.</h5>`;
 
     book.docs.forEach(book => {
         const coverImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         console.log(book.cover_i);
-        /* if(coverImage === null){
-            coverImageLink = 'https://i.imgur.com/sJ3CT4V.gif'
-        }else{
-            coverImageLink = coverImage;
-        } */
         const coverImg = () => {
             let coverImageLink = '';
             if(book.cover_i == undefined){
@@ -39,13 +42,13 @@ const bookInfo = (book) => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
-        <h5 class="card-title mx-auto mt-2">Book: ${book.title}</h5>
+        <div class="card img-thumbnail">
+        <h5 style="max-width:75%" class="card-title mx-auto mt-2">Book: ${book.title}</h5>
             <img height=400 src="${coverImg()}" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Author: ${book.author_name[0]}</p>
                 <p class="card-text">Publisher: ${book.publisher}</p>
-                <p class="card-text">First Publish: ${book.publish_date[book.publish_date.length - 1]}</p>
+                <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
             </div>
         </div>
         `;
