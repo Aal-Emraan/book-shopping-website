af2863389d983740be0108c6da92e73daafdc65e
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-02 20:16:36 +0600

    updated

--- js/book.js
+++ js/book.js
@@ -7,12 +7,12 @@ let search = '';
 
 
 
-const getBooks = () => {
+/* const getBooks = () => {
     const url = 'http://openlibrary.org/search.json?q=javascript';
     fetch(url)
     .then(res => res.json())
     .then(data => console.log(data))
-}
+} */
 
 const showSearchedBooks = () => {
     
@@ -45,17 +45,17 @@ const bookInfo = (book) => {
 
     if(book.num_found === 0){
         searchNumber.innerHTML = `
-        <h5 class="mx-4 my-3 text-danger">no result found for <i class='fs-3 text-secondary'>${search}</i> 😪</h5>`;
+        <h5 class="mx-4 my-3 text-danger">No result found for <i class='fs-3 text-secondary'>"${search}"</i> 😪</h5>`;
     }else{
         searchNumber.innerHTML = `
-        <h5 class="mx-4 my-3 text-success">${book.num_found} results found for <i class='fs-3 text-primary'>${search}.</i></h5>`;
+        <h5 class="mx-4 my-3 text-info">${book.num_found} results found for <i class='fs-3 text-warning'>"${search}"</i> 😎</h5>`;
     }
 
-    book.docs.forEach(book => {
-        const coverImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    book.docs.forEach(bookItem => {
+        const coverImage = `https://covers.openlibrary.org/b/id/${bookItem.cover_i}-M.jpg`;
         const coverImg = () => {
             let coverImageLink = '';
-            if(book.cover_i === undefined){
+            if(bookItem.cover_i === undefined){
                 coverImageLink = 'img/book-na-1.jpg';
             }else{
                 coverImageLink = coverImage;
@@ -65,13 +65,13 @@ const bookInfo = (book) => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card img-thumbnail">
-        <h5 style="max-width:75%" class="card-title mx-auto mt-2">${book.title}</h5>
+        <div class="card img-thumbnail text-white" style="background:#002451">
+        <h5 style="max-width:90%" class="card-title mx-auto mt-2 text-uppercase text-center">${bookItem.title}</h5>
             <img height=300 src="${coverImg()}" class="card-img-top" alt="...">
             <div class="card-body">
-                <p class="card-text">Author: ${book.author_name}</p>
-                <p class="card-text">Publisher: ${book.publisher}</p>
-                <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
+                <p class="card-text"><strong>Author:</strong> <i class='text-danger'>${bookItem.author_name}</i></p>
+                <p style="max-height: 100px;overflow:hidden" class="card-text"><strong class="text-warning">Publisher: </strong>${bookItem.publisher}</p>
+                <p class="card-text"><strong class="text-warning">First Publish Year: </strong>${bookItem.first_publish_year}</p>
             </div>
         </div>
         `;
